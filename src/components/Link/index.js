f52cbec446532ba82a6bf650be77ad9a2cefab7b
Jonic Linley <jonic@codeclub.org.uk>
2017-10-16 13:00:43 +0100

    First attempt at making Bits react-compatible

---
+++ src/components/Link/index.js
@@ -0,0 +1,22 @@
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+// import './Link.scss'
+
+const Link = ({ className, to, ...props }) => {
+  const classes = classnames('c-link', className)
+
+  return (
+    <RouterLink className={classes} to={to} {...props}>
+      {children}
+    </RouterLink>
+  )
+}
+
+Link.propTypes = {
+  className: PropTypes.string,
+  to: PropTypes.string.isRequired,
+}
+
+export default Link
