bb4beb8116fbc06b8ce7bc4ad1bd3dba4fdda8b1
Edward Andrews-Hodgson <edward@aldreth.com>
2017-10-27 09:26:20 +0100

    Change setup slightly

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,34 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.js',
+  input: 'src/components/index.js',
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
+      format: 'cjs'
     },
+    {
+      file: pkg.module,
+      format: 'es'
+    }
   ],
-  external: ['react', 'react-dom', 'prop-types'],
-  plugins: [resolve(), babel(), commonjs()],
+  external: [
+    'react',
+    'react-dom',
+    'prop-types'
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    postcss({
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]
 }
