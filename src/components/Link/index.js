b6f12c6e983f8deddecb53aaa5342b9409ac9628
Jonic Linley <jonic@raspberrypi.org>
2017-11-02 12:37:11 +0000

    LINTING

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,12 +1,12 @@
-import classnames from "classnames"
-import PropTypes from "prop-types"
-import React from "react"
-import { Link as RouterLink } from "react-router-dom"
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
 
-import "./Link.scss"
+import './Link.scss'
 
 const Link = ({ children, className, to, ...props }) => {
-  const classes = classnames("c-link", className)
+  const classes = classnames('c-link', className)
 
   return (
     <RouterLink className={classes} to={to} {...props}>
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,4 @@
 import Button from './Button'
 import Link from './Link'
 
-export {
-  Button,
-  Link
-}
+export { Button, Link }
