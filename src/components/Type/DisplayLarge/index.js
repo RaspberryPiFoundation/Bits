03e32efcb3225ba7bfea1fddf7ce30796fb10da2
Jonic Linley <jonic@raspberrypi.org>
2017-11-23 16:54:53 +0000

    Added Type components

---
+++ src/components/Type/DisplayLarge/index.js
@@ -0,0 +1,29 @@
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+import React from 'react'
+import Type from '../'
+
+import './DisplayLarge.scss'
+
+export const TypeDisplayLarge = ({
+  children,
+  className,
+  element,
+  ...props
+}) => {
+  const classNames = classnames('c-type-display-large', className)
+
+  return (
+    <Type className={classNames} element={element} {...props}>
+      {children}
+    </Type>
+  )
+}
+
+TypeDisplayLarge.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  element: PropTypes.string,
+}
+
+export default TypeDisplayLarge
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,26 @@ import Button from './Button'
 import Card from './Card'
 import Link from './Link'
 import Pagination from './Pagination'
+import Type from './Type'
+import TypeBase from './Type/Base'
+import TypeCaption from './Type/Caption'
+import TypeDisplay from './Type/Display'
+import TypeDisplayLarge from './Type/DisplayLarge'
+import TypeHeading from './Type/Heading'
+import TypeSubHeading from './Type/SubHeading'
 import Wysiwyg from './Wysiwyg'
 
-export { Button, Card, Link, Pagination, Wysiwyg }
+export {
+  Button,
+  Card,
+  Link,
+  Pagination,
+  Type,
+  TypeBase,
+  TypeCaption,
+  TypeDisplay,
+  TypeDisplayLarge,
+  TypeHeading,
+  TypeSubHeading,
+  Wysiwyg,
+}
