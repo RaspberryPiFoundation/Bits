927cda493d17b3c1efc1c86d25c7137356b311c2
Jonic Linley <jonic@raspberrypi.org>
2017-12-08 13:31:08 +0000

    Added button—light-on-dark class

--- stories/components/Button/stories.js
+++ stories/components/Button/stories.js
@@ -3,13 +3,33 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 
 import Button from '../../../src/components/Button'
+import LightOnDark from '../../../src/components/LightOnDark'
+import Slice from '../../../src/components/Slice'
 
 storiesOf('Button', module)
   .add('Primary', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+    <Slice>
+      <Button>Hello Button</Button>
+    </Slice>
   ))
   .add('Secondary', () => (
-    <Button onClick={action('clicked')} className="c-button--secondary">
-      Hello Button
-    </Button>
+    <Slice>
+      <Button className="c-button--secondary">Hello Button</Button>
+    </Slice>
+  ))
+  .add('Primary on dark background', () => (
+    <Slice uBgcolor="ui-black">
+      <LightOnDark>
+        <Button className="c-button--light-on-dark">Hello Button</Button>
+      </LightOnDark>
+    </Slice>
+  ))
+  .add('Secondary on dark background', () => (
+    <Slice uBgcolor="ui-black">
+      <LightOnDark>
+        <Button className="c-button--light-on-dark c-button--secondary">
+          Hello Button
+        </Button>
+      </LightOnDark>
+    </Slice>
   ))
