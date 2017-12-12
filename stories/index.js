c0e0950fda7fedd3ae6b035b648a5fb6c433b366
Jonic Linley <jonic@raspberrypi.org>
2017-12-12 20:57:34 +0000

    Removed Storybook stuff

--- stories/index.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react'
-
-const components = ['Button', 'Card', 'Link']
-
-configure(() => {
-  for (let component of components) {
-    require(`./components/${component}/stories.js`) // eslint-disable-line global-require
-  }
-}, module)
