42577b74b4d601ae1f43e5d9f81f5c055fe98194
Jonic Linley <jonic@raspberrypi.org>
2018-01-31 10:57:05 +0000

    Wrote tests for Link component

---
+++ src/components/Link/Link.test.js
@@ -0,0 +1,69 @@
+import { mount, shallow } from 'enzyme'
+import { Link as RouterLink } from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom'
+import Link from '../Link'
+import React from 'react'
+
+let internalLink
+
+const children = 'Whatever'
+const internalTo = '/path/to/wherever'
+const externalTo = 'https://www.100yen.co.uk'
+
+describe('<Player />', () => {
+  describe('Renders...', () => {
+    beforeEach(() => {
+      internalLink = shallow(<Link to={internalTo}>{children}</Link>)
+    })
+
+    it('without crashing', () => {
+      expect(internalLink.find('.c-link')).toBePresent()
+    })
+
+    it('matches its snapshot', () => {
+      expect(internalLink).toMatchSnapshot()
+    })
+
+    describe('external links', () => {
+      it('renders a React Router Link component', () => {
+        let externalLink = mount(
+          <MemoryRouter>
+            <Link to={externalTo}>{children}</Link>
+          </MemoryRouter>,
+        )
+        expect(externalLink.find(RouterLink)).toBePresent()
+      })
+    })
+  })
+
+  describe('Modifiers', () => {
+    describe('lightOnDark', () => {
+      it('adds the correct className', () => {
+        internalLink = shallow(
+          <Link lightOnDark to={internalTo}>
+            {children}
+          </Link>,
+        )
+
+        expect(internalLink.find('.c-link')).toHaveClassName(
+          'c-link--light-on-dark',
+        )
+      })
+    })
+
+    describe('excludeClassname', () => {
+      it("doesn't have any classNames", () => {
+        internalLink = shallow(
+          <Link excludeClassName lightOnDark to={internalTo}>
+            {children}
+          </Link>,
+        )
+
+        expect(internalLink.find('a')).not.toHaveClassName(
+          'c-link',
+          'c-link--light-on-dark',
+        )
+      })
+    })
+  })
+})
