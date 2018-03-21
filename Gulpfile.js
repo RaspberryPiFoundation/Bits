d467a99fb24a63008dddcf7feb508e905d0e6053
Jonic Linley <jonic@100yen.co.uk>
2018-03-21 14:29:34 +0000

    V1.1.2 (#69)
    * Added Gulp stuff back in. Not sure why it was removed!
    * Ignoring ImportPath scss-link rule
    * Fixes #66 - border utility allows “none”
    * Fixed #67 - box shadow utility allows none
    * Fixes #65 - cards now 100% width by default
    * Slice has default bgcolor
    * Added rpi style links

---
+++ Gulpfile.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const gulp = require('gulp')
+const pkg = require('./package.json')
+
+const autoprefixer = require('gulp-autoprefixer')
+const cssnano = require('gulp-cssnano')
+const header = require('gulp-header')
+const notify = require('gulp-notify')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
+const scsslint = require('gulp-scss-lint')
+const sourcemaps = require('gulp-sourcemaps')
+const stripComments = require('gulp-strip-css-comments')
+
+// Set banner template
+const banner = [
+  '/*',
+  `  ${pkg.name}`,
+  '',
+  `  ${pkg.description}`,
+  '',
+  `  Author:      ${pkg.author.name}`,
+  `  Author URI:  ${pkg.author.url}`,
+  `  Version:     ${pkg.version}`,
+  '*/',
+  '',
+  '',
+].join('\n')
+
+// Get configuration files
+const assetsConfig = {
+  stylesheets: {
+    main: './src/styles/main.scss',
+    destDir: './lib',
+    buildFile: 'Bits.css',
+    buildFileMin: 'Bits.min.css',
+  },
+}
+
+gulp.task('compile_all', ['compile_stylesheets'])
+
+gulp.task('compile_stylesheets', () => {
+  let config = assetsConfig.stylesheets
+
+  return gulp
+    .src([config.main])
+    .pipe(
+      sass().on('error', err => {
+        return notify().write(err)
+      }),
+    )
+    .pipe(stripComments())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+      }),
+    )
+    .pipe(
+      header(banner, {
+        buildFile: config.buildFile,
+        pkg: pkg,
+      }),
+    )
+    .pipe(rename(config.buildFile))
+    .pipe(gulp.dest(config.destDir))
+    .pipe(sourcemaps.init())
+    .pipe(cssnano())
+    .pipe(rename(config.buildFileMin))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(config.destDir))
+    .pipe(notify(`${pkg.name} Gulp: stylesheets completed`))
+})
+
+gulp.task('scsslint', () => {
+  return gulp.src('src/**/*.scss').pipe(scsslint())
+})
+
+gulp.task('watch', () => {
+  gulp.watch('./src/**/*.scss', ['scsslint', 'compile_stylesheets'])
+})
+
+gulp.task('default', ['scsslint', 'compile_all', 'watch'])
