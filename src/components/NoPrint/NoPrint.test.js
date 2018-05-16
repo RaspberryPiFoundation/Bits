122ccea5d50eaa79c63aa1d61c84a89b9101309f
Jonic Linley <jonic@codeclub.org.uk>
2018-05-16 11:55:50 +0100

    Added NoPrint component

---
+++ src/components/NoPrint/NoPrint.test.js
@@ -0,0 +1,31 @@
+import { shallow } from 'enzyme'
+import NoPrint from '../NoPrint'
+import React from 'react'
+
+let noPrint
+
+const children = 'Whatever'
+const className = 'some-extra-class-name'
+
+describe('<Slice />', () => {
+  describe('Renders...', () => {
+    beforeEach(() => {
+      noPrint = shallow(<NoPrint>{children}</NoPrint>)
+    })
+
+    it('without crashing', () => {
+      expect(noPrint.find('.u-no-print')).toBePresent()
+    })
+
+    it('matches its snapshot', () => {
+      expect(noPrint).toMatchSnapshot()
+    })
+  })
+
+  describe('With classNames', () => {
+    it('adds the correct className', () => {
+      noPrint = shallow(<NoPrint className={className}>{children}</NoPrint>)
+      expect(noPrint.find('.u-no-print')).toHaveClassName(className)
+    })
+  })
+})
